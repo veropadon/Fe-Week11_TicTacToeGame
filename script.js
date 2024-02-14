05f78b14655a754f1b266a67d972b34221091d21
Veronica Padon <104039231+veropadon@users.noreply.github.com>
2024-02-14 14:07:49 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,81 @@
+$(document).ready (function ()  {
+  let currentPlayer = "X";
+  let moves = 0;
+  $('#resetButton').click(function() {
+    restartGame();
+
+  });
+
+  $("#ticTacToeGrid .grid-cell").click(function () {
+    if($(this).text() === "" && !gameOver()) {
+        $(this).text(currentPlayer); 
+        moves++;
+      if(checkWinner(currentPlayer)) {
+        showAlert(currentPlayer + " " +"Wins!");
+        return;
+      }else if (moves === 9) {
+        showAlert('Draw!');
+        return;
+
+      }
+    
+// if player click on a grid cell switch current Player from X to O 
+
+   currentPlayer = currentPlayer === "X" ? "0" : "X";
+   $("#turnIndicator").text(currentPlayer + " 's turn");
+  
+
+
+    }
+
+
+
+  });
+
+
+  function checkWinner(player) {
+    const winPatterns = [
+       [0,1,2] ,    
+       [3,4,5] ,
+       [6,7,8] ,    //Rows
+       [0,3,6] ,    
+       [1,4,7] ,
+       [2,5,8] ,    //Columns 
+       [0,4,8] ,
+       [2,4,6]     //Diagonals
+    ];
+
+    return winPatterns.some((pattern) => {
+        return pattern.every((index) => {
+            return $(`#ticTacToeGrid .grid-cell`).eq(index).text() === player;
+        });
+    });
+}
+
+  
+  function restartGame() {
+    $("#ticTacToeGrid .grid-cell").text("");
+    currentPlayer = "X";
+    $('#turnIndicator').text("X's turn");
+    moves = 0;
+    $('.alert').alert("close")
+
+  }
+
+  function showAlert(message) {
+    const alertHTML = `<div class="alert alert-success" role="alert">  ${message} </div>`;
+     $('.container').prepend(alertHTML);
+
+  }
+
+
+
+  function gameOver(){
+  return $('.alert').length > 0;
+  }
+
+})
+
+
+
+
